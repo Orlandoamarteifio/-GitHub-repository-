bd89e0d765ec94ca0d533846bdbd34f9574b3e9e
ORLANDO AMARTEIFIO <seniorcorradi1234@gmail.com>
2023-02-28 18:36:17 +0530

    Update 14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,23 +1,34 @@
 #include "binary_trees.h"
 
 /**
- * array_to_heap - builds a Max Binary Heap tree from an array
- * @array: input array
- * @size: size of array
- * Return: pointer to the root node of the created Binary Heap,
- * or NULL on failure
+ * binary_tree_balance - Measures the balance factor of a binary tree.
+ * @tree: A pointer to the root node of the tree to measure the balance factor.
+ *
+ * Return: If tree is NULL, return 0, else return balance factor.
  */
-heap_t *array_to_heap(int *array, size_t size)
+int binary_tree_balance(const binary_tree_t *tree)
 {
-	size_t i = 0;
-	bst_t *root = NULL;
+	if (tree)
+		return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
 
-	if (!array)
-		return (NULL);
-	while (i < size)
+	return (0);
+}
+
+/**
+ * binary_tree_height - Measures the height of a binary tree.
+ * @tree: A pointer to the root node of the tree to measure the height.
+ *
+ * Return: If tree is NULL, your function must return 0, else return height.
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (tree)
 	{
-		heap_insert(&root, array[i]);
-		i++;
+		size_t l = 0, r = 0;
+
+		l = tree->left ? 1 + binary_tree_height(tree->left) : 1;
+		r = tree->right ? 1 + binary_tree_height(tree->right) : 1;
+		return ((l > r) ? l : r);
 	}
-	return (root);
+	return (0);
 }
